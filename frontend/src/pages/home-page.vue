1232c61c32d84f9dcf9c49fd732fbfbd4bee1629
IvanZemsky <i.ivanzemsky2005@gmail.com>
2025-09-21 16:20:40 +0300

    feat: initially add basic components

--- frontend/src/pages/home-page.vue
+++ frontend/src/pages/home-page.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { UiButton } from "@/shared/ui"
-import { Star } from "lucide-vue-next"
+
 </script>
 
 <template>
   <div>
     Homepage
-    <UiButton variant="round" class="w-10 h-10">
-      <Star fill="currentColor"/>
-    </UiButton>
+
   </div>
 </template>
 
--- frontend/src/shared/ui/index.ts
+++ frontend/src/shared/ui/index.ts
@@ -1,3 +1,25 @@
 import UiButton from "./kit/button/UiButton.vue"
+import UiCard from "./kit/card/UiCard.vue"
+import CardAction from "./kit/card/CardAction.vue"
+import CardContent from "./kit/card/CardContent.vue"
+import CardDescription from "./kit/card/CardDescription.vue"
+import CardFooter from "./kit/card/CardFooter.vue"
+import CardHeader from "./kit/card/CardHeader.vue"
+import CardTitle from "./kit/card/CardTitle.vue"
+import UiProgress from "./kit/progress/UiProgress.vue"
+import UiInput from "./kit/input/UiInput.vue"
+import UiTextarea from "./kit/textarea/UiTextarea.vue"
 
-export { UiButton }
+export {
+  UiButton,
+  UiCard,
+  UiProgress,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  UiInput,
+  UiTextarea,
+}
